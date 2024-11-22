03a87b71541648668c4d5665a284b606b39b8d92
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-22 14:08:08 +0100

    feat(main): add support for processing multiple file types and verbose mode
    - Introduced `update_file` to handle different file types (`epub`, `zip`, `pdf`, `mobi`) dynamically using dedicated update functions.
    - Added `--verbose` flag for conditional verbose output, updating state and logging errors or detailed information.
    - Improved argument parsing for better clarity and functionality.
    - Updated `initialize_state` to validate and set default image formats.
    - Enhanced error handling and output for unsupported file types.

--- FicImageScript/main.py
+++ FicImageScript/main.py
@@ -1,7 +1,17 @@
 import argparse
 import sys
-from .shared_state import state
-from .utils import config_check, load_config_json, file_search, check_for_update, update_file
+from .global_state import state
+from .utils import (
+    config_check,
+    load_config_json,
+    file_search,
+    check_for_update,
+    identify_file_type,
+)
+from .epub import update_epub
+from .zip import update_zip
+from .pdf import update_pdf
+from .mobi import update_mobi
 
 
 def initialize_state(config_file_path: str) -> None:
@@ -15,37 +25,62 @@ def initialize_state(config_file_path: str) -> None:
         ficimage_config = load_config_json(config_file_location)
 
         # Update state with values from the config file
-        state["compress_images"] = ficimage_config.get("compress_images", state["compress_images"])
-        state["default_image_format"] = ficimage_config.get("default_image_format", state["default_image_format"])
+        state["compress_images"] = ficimage_config.get(
+            "compress_images", state["compress_images"]
+        )
+        state["default_image_format"] = ficimage_config.get(
+            "default_image_format", state["default_image_format"]
+        )
 
         # Validate the image format
         if state["default_image_format"].lower() not in ("jpg", "jpeg", "png"):
             state["default_image_format"] = "JPEG"
 
-        state["max_image_size"] = ficimage_config.get("max_image_size", state["max_image_size"])
+        state["max_image_size"] = ficimage_config.get(
+            "max_image_size", state["max_image_size"]
+        )
+
+
+def update_file(file_path: str):
+    file_type = identify_file_type(file_path)
+    update_function = None
+
+    if file_type == "epub":
+        update_function = update_epub
+    elif file_type == "zip":
+        update_function = update_zip
+    elif file_type == "pdf":
+        update_function = update_pdf
+    elif file_type == "mobi":
+        update_function = update_mobi
+
+    if update_function:
+        update_function(file_path)
+    else:
+        print(f"Unsupported file type: {file_type}")
 
 
 def main() -> None:
     """
-    This function updates the FicHub epub file with images.
+    This function updates the FicHub file with images.
 
     :return: None
     """
 
-    parser = argparse.ArgumentParser(
-        description="Update a FicHub file with images.")
-    parser.add_argument("-p", "--path",
-                        help="The path to the FicHub file.")
-    parser.add_argument("-c", "--config_file_path",
-                        help="The path to the ficimage.json file.")
+    parser = argparse.ArgumentParser(description="Update a FicHub file with images.")
+    parser.add_argument("-p", "--path", help="The path to the FicHub file.")
     parser.add_argument(
-        "-d", "--debug", help="Enable debug mode.", action="store_true")
+        "-c", "--config_file_path", help="The path to the ficimage.json file."
+    )
+    parser.add_argument(
+        "-V", "--verbose", help="Enable verbose output", action="store_true"
+    )
     parser.add_argument(
         "-v",
         "--version",
         help="Prints out the current version and quits.",
-        action='version',
-        version=f"FicImage Version {state['version']}"
+        action="version",
+        version=f"FicImage Version {state['version']}",
     )
     parser.add_argument(
         "-r",
@@ -53,27 +88,33 @@ def main() -> None:
         help="This will update all files in the directory path given and its subdirectories.",
     )
     parser.add_argument(
-        "-u", "--update",
+        "-u",
+        "--update",
         help="Check if a new version is available.",
-        action="store_true"
+        action="store_true",
     )
     args = parser.parse_args()
 
     file_path = args.path
     config_file_path = args.config_file_path
-    debug = args.debug
+    verbose = args.verbose
     recursive = args.recursive
 
     if args.config_file_path:
         initialize_state(config_file_path)
 
+    if verbose:
+        state["verbose"] = verbose
+
     if args.update:
-        check_for_update(state['version'])
+        check_for_update(state["version"])
         sys.exit()
 
     if file_path is None and recursive is None:
-        sys.exit("Either pass in a path to a FicHub file or use the --recursive flag to convert "
-                 "the current directory and its sub-directories")
+        sys.exit(
+            "Either pass in a path to a FicHub file or use the --recursive flag to convert "
+            "the current directory and its sub-directories"
+        )
     elif file_path:
         if recursive:
             print("Ignoring --recursive flag since file_path was given")
@@ -85,9 +126,9 @@ def main() -> None:
                 update_file(i)
             except Exception as e:
                 print(f"Error! Skipping {i}")
-                if debug:
+                if verbose:
                     print(f"Exception: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
